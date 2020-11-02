754ef922f4d7d003f6710b157ab887c9acce49c3
Andrii Konovalenko <an256.ak@gmail.com>
2020-11-02 16:19:28 +0200

    feat: added burger-menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,3 +1,42 @@
+// burger-menu
+const burgerMenu = () => {
+	const burgerBtn = document.getElementById('burger'),
+  navigation = document.querySelector('.navigation'),
+  logo = document.querySelector('.logo'),
+  body = document.querySelector('body');
+
+  const handlerMenu = () => {
+		burgerBtn.classList.toggle('burger--active');
+    navigation.classList.toggle('navigation-hide');
+	};
+
+  body.addEventListener('click', event => {
+    let target = event.target;
+
+    if (target.matches('#burger')) {
+      handlerMenu();
+    } 
+
+    if (target.matches('.navigation')) {
+      burgerBtn.classList.remove('burger--active');
+      navigation.classList.add('navigation-hide');
+      console.log('Поехали');
+    }
+  });
+  
+  // navigation.addEventListener('click', event => {
+	// 	event.preventDefault();
+	// 	const target = event.target;
+
+	// 	if (target.matches('a[href*="#"]')) {
+	// 		burgerBtn.classList.toggle("burger--active");
+  //     navigation.classList.toggle('navigationr-hide');
+	// 	}
+  // });
+};
+
+burgerMenu();
+
 //pets slider
 const petsSlider = () => {
   new Swiper('.pets__slider', {
@@ -23,3 +62,4 @@ const petsSlider = () => {
 }
 
 petsSlider();
+
